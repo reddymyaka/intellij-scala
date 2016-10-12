81f52ffc5cd0eb2cc077a372ebb75df095b5cfef
Justin Kaeser <justin.kaeser@jetbrains.com>
2016-10-12 11:52:09 +0200

    support some more or less common completions in sbt shell.
    This is just a show-case. Proper completion should depend on data directly from the server shell and also utilise the parsers that commands and input tasks provide, as well as use syntactic hints

--- src/org/jetbrains/sbt/shell/SbtShellCompletionContributor.scala
+++ src/org/jetbrains/sbt/shell/SbtShellCompletionContributor.scala
@@ -21,8 +21,25 @@ object SbtShellCompletionProvider extends CompletionProvider[CompletionParameter
   override def addCompletions(parameters: CompletionParameters,
                               context: ProcessingContext,
                               result: CompletionResultSet): Unit = {
-    val elem = LookupElementBuilder.create("dingsbums")
-    result.addElement(elem)
+
+    // TODO just some commonly used builtins for now. later, load all the tasks, settings, commands, projects etc from sbt server and give more complete suggestions based on that!
+    val tasks = Seq(
+      "compile", "test", "console", "clean", "update", "updateClassifiers", "updateSbtClassifiers",
+      "publish", "publishLocal", "consoleProject")
+
+    val settings = Seq("libraryDependencies", "baseDirectory", "sourceDirectory", "unmanagedBase", "target")
+
+    // TODO figure out how to use the parsers for InputKeys and Commands to supply their autocompletion to shell
+    val inputs = Seq("run", "runMain", "testOnly")
+    val commands = Seq("help", "reload", "plugins", "settings", "project", "projects")
+
+    val all = tasks ++ settings ++ inputs ++ commands
+
+    all.foreach { key =>
+      val elem = LookupElementBuilder.create(key)
+      result.addElement(elem)
+    }
+
   }
 
 }
