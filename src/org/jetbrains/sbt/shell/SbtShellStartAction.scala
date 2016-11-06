a12b5247a9ec9ebdc54b2f191d26a774ebc77228
Justin Kaeser <justin.kaeser@jetbrains.com>
2016-11-06 18:01:07 +0100

    shell button in tool window

--- src/org/jetbrains/sbt/shell/SbtShellStartAction.scala
+++ src/org/jetbrains/sbt/shell/SbtShellStartAction.scala
@@ -1,11 +1,16 @@
 package org.jetbrains.sbt.shell
 
 import com.intellij.openapi.actionSystem.{AnAction, AnActionEvent}
+import org.jetbrains.plugins.scala.icons.Icons
 
 /**
   * Created by jast on 2016-11-04.
   */
 class SbtShellStartAction extends AnAction("Run SBT Shell") {
+
+  override def update(e: AnActionEvent): Unit =
+    e.getPresentation.setIcon(Icons.SCALA_CONSOLE)
+
   override def actionPerformed(event: AnActionEvent): Unit = {
     SbtShellRunner.run(event.getProject)
   }
